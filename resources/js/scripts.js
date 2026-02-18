2bca4365196e398b76bd4df8fdaed2f28927aa1e
Philip Waern <waernph@gmail.com>
2026-02-18 14:20:32 +0100

    Created style-ligt.css.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,11 +1,7 @@
-function swapDarkLightStyle(styleDark, styleLight){
-    document.getElementById(styleDark).setAttribute('href',styleDark);
-    document.getElementById(styleLight).setAttribute('href', styleLight);
+
+function year(){
+    const year = new Date().getFullYear();
+    document.getElementById("yearNow").innerText = year;
 }
 
-function yearNow(){
-    const year = new Date().getFullYear();
-    let yearString = year.toString();
-    document.getElementById("yearNow").innerText = yearString;
-    return yearString;
-}
+year();
