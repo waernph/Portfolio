088037db37e702097ec08d7053283a9811342847
Philip Waern <waernph@gmail.com>
2026-02-21 19:21:59 +0100

    Changes in fonts. Changes in nav

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,10 @@
+var contactModal = document.getElementById("contact-modal");
+var openButton = document.getElementById("button-open-modal");
+var closeButton = document.getElementsByClassName("button-close-modal");
+
 
 function year(){
     const year = new Date().getFullYear();
     document.getElementById("yearNow").innerText = year;
 }
-
 year();
